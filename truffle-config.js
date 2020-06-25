00edd4e69559a990668b39e658a12251317ccc48
Prateek Sawhney <Prateek.sawhney97@gmail.com>
2020-06-25 22:35:59 +0530

    updated truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,16 @@
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  compilers: {
+    solc: {
+      version: "0.4.17" // A version or constraint - Ex. "^0.5.0"
+    }
+  },
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 9545,
+      network_id: "5777" // Match any network id
+    }
+  }
 };
